b93c5ec982217b13abfbe056594e97d229128b5c
igot-apps <aagortey@gmail.com>
2023-08-10 12:35:02 +0000

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+/**
+* main - this is the starting or main entry of the script
+*
+* Return - it returns 0 Success when success
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    	return (1);
 }
