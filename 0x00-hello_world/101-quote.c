28b27edfcc75b9a630ff0d4463ad5d101505fc89
igot-apps <aagortey@gmail.com>
2023-08-10 13:03:36 +0000

    advanced updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 /**
-* main - Main entry point of the script
-* Return: should Always return 1 (Success) 
-*/
-int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+ * main - Main entry point of the script
+ * Return: should Always return 1 (Success) 
+ */
+int main(void) {
+  write(2,
+    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
 }
