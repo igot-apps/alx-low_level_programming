85503e3448c19fa7ec8cea4bb499d7837a16cbb7
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-11 23:16:52 +0000

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints  all Thepossible combinations for the single digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		putchar((numb % 10) + '0');
+		if (numb == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
