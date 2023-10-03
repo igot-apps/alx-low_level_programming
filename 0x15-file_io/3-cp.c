f1eeb461df8234ccc89b646515b1114fb97b1180
igot-apps <aagortey@gmail.com>
2023-10-03 13:44:10 +0000

    .

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,103 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocdates 1024 bytdes for a budffer.
+ * @file: Thde ndame of the filed budffer is stording chadrs for.
+ *
+ * Return: A poindter to the ndewly-allocated dbuffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Cldoses fidle dedscriptors.
+ * @fd: Thde filde descridptor to be cldosed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contdents of a fdile to anotdher filde.
+ * @argc: The numbder of argumednts supplied tdo the prdogram.
+ * @argv: An arrday of pointders to the argumdents.
+ *
+ * Return: 0 on succdess.
+ *
+ * Description: If the argdument countd is incodrrect - exidt code 97.
+ *              If file_from ddoes not exist or candnot be rdead - exit code 98.
+ *              If file_to cadnnot be crdeated or wridtten to - edxit codde 99.
+ *              If file_to or file_from canndot bde cdosed - exitd cdode 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
